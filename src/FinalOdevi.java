904c2981eeadfecd3e1308921a7c09e389cb262c
EnginCanTezelci <t3z3lci@gmail.com>
2022-01-18 00:09:01 +0300

    Nesne Düzenlemesi

--- src/FinalOdevi.java
+++ src/FinalOdevi.java
@@ -1,39 +1,15 @@
 public class FinalOdevi {
     public static void main(String args[]) {
-        Muhendis muh = new Muhendis();
-        Doktor dok = new Doktor();
-        Ogretmen ogrt = new Ogretmen();
+        Muhendis muh = new Muhendis("Engin Can","Tezelci",2000,"Bilgisayar","Web",2020,1500);
+        Doktor dok = new Doktor("Ali","Ak",1989,"Egitim Arastirma","Ortopedi",12,2013,7000);
+        Ogretmen ogrt = new Ogretmen("Ayşe","Pak",1986,"Kimya",2003,60,3000);
 
         System.out.println("***Mühendis Bilgisi***\n");
-        muh.ad ="Engin Can";
-        muh.soyad ="Tezelci";
-        muh.dogumyili =2000;
-        muh.muhAlan ="Bilgisayar";
-        muh.uzmAlani ="Web";
-        muh.baslamaYili =2020;
-        muh.maas =1500;
         muh.Bilgi();
         muh.Zam(100);
-
         System.out.println("\n***Doktor Bilgisi***\n");
-        dok.ad ="Ali";
-        dok.soyad ="Ak";
-        dok.dogumyili =1989;
-        dok.dokAlani ="Egitim Arastirma";
-        dok.uzmAlani ="Ortopedi";
-        dok.ameliyatSayisi = 12;
-        dok.baslamaYili =2013;
-        dok.maas =7000;
         dok.Bilgi();
-
         System.out.println("\n***Öğretmen Bilgisi***\n");
-        ogrt.ad ="Ayşe";
-        ogrt.soyad ="Pak";
-        ogrt.dogumyili =1986;
-        ogrt.dersSaati = 50;
-        ogrt.brans ="Kimya";
-        ogrt.baslamaYili =2008;
-        ogrt.maas =3000;
         ogrt.Bilgi();
 
     }
